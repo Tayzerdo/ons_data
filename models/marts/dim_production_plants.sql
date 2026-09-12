f1ffb8d596863b39b9f47bb93af4287d98aa7a4f
Tayzer <tayzer@futurehome.no>
2026-09-12 15:52:31 +0100

    update process

--- models/marts/dim_production_plants.sql
+++ models/marts/dim_production_plants.sql
@@ -1,5 +1,5 @@
 with staging_data as (
-    select * from {{ ref('stg_modalidade_usina') }} -- your renamed model
+    select * from {{ ref('stg_production_plants') }} -- your renamed model
 )
 
 select *
