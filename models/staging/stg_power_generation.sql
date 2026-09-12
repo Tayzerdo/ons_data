392d3649353e21039029fe6cb5403058c7e32b57
Tayzer <tayzer@futurehome.no>
2026-09-12 14:38:51 +0100

    update process

--- models/staging/stg_power_generation.sql
+++ models/staging/stg_power_generation.sql
@@ -7,10 +7,11 @@ WITH CTE AS (
 
 SELECT
     din_instante AS dtm_power_generation,
-    id_subsistema AS id_subsystem,
-    nom_subsistema AS dsc_subsystem_name,
     nom_tipousina AS dsc_plant_type,
     nom_tipocombustivel AS dsc_fuel_type,
+    nom_usina AS dsc_plant_name,
+    id_subsistema AS id_subsystem,
+    nom_subsistema AS dsc_subsystem_name,
     val_geracao AS val_power_generation
 
 FROM CTE
